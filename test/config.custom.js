dd43400af75d0257a35f6e7d14e0688dffe25c98
em <michael@joinbox.com>
2015-10-19 16:35:13 +0200

    added support for custom environments

---
+++ test/config.custom.js
@@ -0,0 +1,6 @@
+
+
+
+    module.exports = {
+        c: 999
+    };
--- test/default.js
+++ test/default.js
@@ -1,11 +1,18 @@
+(function() {
+	'use strict';
 
-	
+
+
 	var   Class 		= require('ee-class')
 		, log 			= require('ee-log')
-		, assert 		= require('assert');
+		, assert 		= require('assert')
+		;
+
+
 
 
 	process.env.EE_ENV_TESTING = true;
+
 
 
 	var ConfigLoader = require('../')
@@ -13,17 +20,34 @@
 
 
 
-	describe('The ConfigLoader', function(){
-		it('should not crash when instantiated', function(){
+	describe('The ConfigLoader', function() {
+		it('should not crash when instantiated', function() {
 			instance = new ConfigLoader(__dirname, __dirname);
 		});	
 
-		it('should return the correct values', function(){
+		it('should return the correct values', function() {
 			assert.equal(JSON.stringify(instance.getAll()), '{"a":1,"b":10}');
 		});
 
-		it('should report the correct status', function(){
+		it('should report the correct status', function() {
 			assert.equal(instance.get('a'), 1);
 		});		
-	});
+
+
+
+
+		it('should report the correct status', function() {
+			ConfigLoader.add('custom', 'live');
+
+			delete process.env.EE_ENV_TESTING;
+			process.env.EE_ENV_CUSTOM = true;
+
+			let instance = new ConfigLoader(__dirname, __dirname);
+
+			assert.equal(JSON.stringify(instance.getAll()), '{"c":999,"b":10}');
+		});
+	});	
+})();
+
+	
 	
